9e9e3bdfce3a21dd6493337b39df77b1f03c1f30
danmakowiesky <dvm1993@hotmail.com>
2023-05-20 10:27:28 -0300

    componentizando

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './style'
+import { Header } from '@components/Header';
+
+export function Groups() {
+  return (
+    <Container>
+      <Header showBackButton />
+    </Container>
+  );
+}
+
