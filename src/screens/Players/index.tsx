ae8f687fdeceffaf8db46c44992fb1c32da631be
danmakowiesky <dvm1993@hotmail.com>
2023-05-29 21:29:18 -0300

    players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,20 +1,68 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native'
+
 import { Header } from "@components/Header";
-import { Container, Form } from "./style";
 import { HighLight } from "@components/HighLight";
 import { ButtonIcon } from "@components/ButtonIcon";
-import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { Input } from "@components/Input";
+import { PlayerCard } from '@components/PlayerCard';
+
+import { Container, Form, HeaderList, NumberOfPlayers } from "./style";
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState(['Rodrigo', 'Vini'])
+
   return (
     <Container>
       <Header showBackButton />
-      <HighLight title="Nome da turma" subTitle="Adicione a galera e separe os times" />
+
+      <HighLight
+        title="Nome da turma"
+        subTitle="adicione a galera e separe os times"
+      />
+
       <Form>
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
-        <ButtonIcon icon="add" />
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+
+        <ButtonIcon
+          icon="add"
+        />
       </Form>
-      <Filter title="Time A" isActive />
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard
+            name={item}
+            onRemove={() => { }}
+          />
+        )}
+      />
     </Container>
   )
 }
--- src/screens/Players/style.ts
+++ src/screens/Players/style.ts
@@ -1,5 +1,4 @@
-import styled from "styled-components/native"
-
+import styled, { css } from "styled-components/native";
 export const Container = styled.View`
   flex: 1;
   background-color: ${({theme}) => theme.COLORS.GRAY_600} ;
@@ -13,3 +12,18 @@ export const Form = styled.View`
   justify-content: center;
   border-radius: 6%;
   `;
+
+  export const HeaderList = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  margin: 32px 0 12px;
+`;
+
+export const NumberOfPlayers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.SM}px;
+  `};
+`;
